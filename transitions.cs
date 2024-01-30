0962b2a5e67275b543bba247e210210a51c05f89
Elly <145195760+ellypaws@users.noreply.github.com>
2024-01-31 06:03:22 +0800

    Implement GetNextAnimation and findTransition naively

--- transitions.cs
+++ transitions.cs
@@ -1,9 +1,30 @@
 #if UNITY_EDITOR
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Linq;
 using UnityEditor.Animations;
+using System.Text.RegularExpressions;
+
+public class RegexClip
+{
+    public string Action { get; set; }
+    public string Char { get; set; }
+    public string ClipNumber { get; set; }
+    public string Alternate { get; set; }
+    public string TransitionTo { get; set; }
+    public string NextName { get; set; }
+    public string NextClip { get; set; }
+}
+
+public class AnimationTransition
+{
+    public string Name { get; set; }
+    public List<string> NextAnimations { get; set; } = new List<string>();
+    public string[] AlternateAnimations { get; set; }
+    public string PreviousAnimation { get; set; }
+}
 
 public class AnimatorStatesLister : EditorWindow
 {
@@ -12,7 +33,7 @@ public class AnimatorStatesLister : EditorWindow
     private int activeLayer = 0;
     private int activeStateMachine = 0;
     private int activeAction = 0;
-    private string[] statesAvailable = { };
+    private int activeClip = 0;
 
     [MenuItem("Tools/Elly/Animator States Lister")]
     public static void ShowWindow()
@@ -60,6 +81,20 @@ public class AnimatorStatesLister : EditorWindow
                 GUILayout.Label(state);
             }
 
+            activeClip = EditorGUILayout.Popup("Clip", activeClip, statesAvailable.ToArray());
+
+            // put activeClip in AnimationTransition class and call GetNextAnimation
+            var clip = new AnimationTransition
+            {
+                Name = statesAvailable.ElementAt(activeClip)
+            };
+            GetNextAnimation(clip, childAnimatorStates);
+            GUILayout.Label("Next Animations");
+            foreach (var nextAnimation in clip.NextAnimations)
+            {
+                GUILayout.Label(nextAnimation);
+            }
+
             if (GUILayout.Button("Figure it out"))
             {
                 DoSomething(layer);
@@ -77,8 +112,132 @@ public class AnimatorStatesLister : EditorWindow
         }
     }
 
+
+    private void GetNextAnimation(AnimationTransition clip, ChildAnimatorState[] allAnimations)
+    {
+        var pattern =
+            @"A_(?<action>[a-z]+)_(?<char>[A-Z]?)_?(?<clip>\d{2})_?(?<alternate>[A-Z]?)?-?(?<transitionTo>(?<nextName>[a-z]+)?_?(?<nextClip>\d{2}))?";
+        var match = Regex.Match(clip.Name, pattern);
+
+        if (!match.Success)
+        {
+            // debug to unity console
+            Debug.Log("No match found for " + clip.Name);
+            return;
+        }
+
+        var result = match.Groups.Cast<Group>().ToDictionary(g => g.Name, g => g.Value);
+
+        if (result["alternate"] != "")
+        {
+            // Alternate clips don't have next animations, but use alternate animations instead
+            return;
+        }
+
+        // Check for transition animations first
+        if (result["transitionTo"] != "")
+        {
+            findTransition(clip, allAnimations, result);
+            return;
+        }
+
+        var nextClipName = string.Format("A_{0}_{1}", result["action"], int.Parse(result["clip"]) + 1);
+        if (result["char"] != "")
+        {
+            nextClipName = string.Format("A_{0}_{1}_{2}", result["action"], result["char"],
+                int.Parse(result["clip"]) + 1);
+        }
+
+        var nextClip = FindAnimationByName($"^{clip.Name}-", allAnimations);
+
+        if (nextClip.Equals(null))
+        {
+            // Try appending "A" or "_A" to the end (e.g., 01 -> 02, 01 -> 02A, 01 -> 02_A)
+            nextClip = FindAnimationByName($"^{nextClipName}_?A?$", allAnimations);
+        }
+
+        if (!nextClip.Equals(null))
+        {
+            // append to clip.NextAnimations
+            clip.NextAnimations.Add(nextClip.GetValueOrDefault().state.name);
+        }
+    }
+
+    private ChildAnimatorState? FindAnimationByName(string expression, ChildAnimatorState[] allAnimations)
+    {
+        return allAnimations.FirstOrDefault(x => Regex.IsMatch(x.state.name, expression));
+    }
+
+    // findTransition finds the next animation based on the transitionTo field.
+    private void findTransition(AnimationTransition clip, ChildAnimatorState[] allAnimations,
+        Dictionary<string, string> result)
+    {
+        // No nextName means transition (e.g., 01-02 -> 02)
+        if (result["nextName"] == "")
+        {
+            // Transition within the same action but different clip number
+            var nextClipName = $"A_{result["action"]}_{result["transitionTo"]}";
+            if (result["char"] != "")
+            {
+                nextClipName = $"A_{result["action"]}_{result["char"]}_{result["transitionTo"]}";
+            }
+            var nextClip = FindAnimationByName($"^{nextClipName}_?A?$", allAnimations);
+            if (nextClip.Equals(null))
+            {
+                // Try appending "A" or "_A" to the end (e.g., 01 -> 02, 01 -> 02A, 01 -> 02_A)
+                nextClip = FindAnimationByName($"^{nextClipName}_?A$", allAnimations);
+            }
+            if (!nextClip.Equals(null))
+            {
+                // append to clip.NextAnimations
+                clip.NextAnimations.Add(nextClip.GetValueOrDefault().state.name);
+            }
+        }
+        else
+        {
+            // With nextName means transition to another action (e.g., 01 -> 01-relax_01 -> relax_01)
+            var nextClipName = $"A_{result["transitionTo"]}";
+            if (result["char"] != "")
+            {
+                nextClipName = $"A_{result["transitionTo"]}_{result["char"]}";
+            }
+            var nextClip = FindAnimationByName($"^{nextClipName}_?A?$", allAnimations);
+            if (nextClip.Equals(null))
+            {
+                // Try appending "A" or "_A" to the end (e.g., 01 -> 02, 01 -> 02A, 01 -> 02_A)
+                nextClip = FindAnimationByName($"^{nextClipName}_?A$", allAnimations);
+            }
+        }
+
+    }
+
     private void DoSomething(AnimatorControllerLayer layer)
     {
+    }
+
+    public RegexClip ParseClipName(string clipName)
+    {
+        var pattern =
+            @"A_(?<action>[a-z]+)_(?<char>[A-Z]?)_?(?<clip>\d{2})_?(?<alternate>[A-Z]?)?-?(?<transitionTo>(?<nextName>[a-z]+)?_?(?<nextClip>\d{2}))?";
+        var match = Regex.Match(clipName, pattern);
+
+        if (!match.Success)
+        {
+            // debug to unity console
+            Debug.Log("No match found for " + clipName);
+            return null;
+        }
+
+        return new RegexClip
+        {
+            Action = match.Groups["action"].Value,
+            Char = match.Groups["char"].Value,
+            ClipNumber = match.Groups["clip"].Value,
+            Alternate = match.Groups["alternate"].Value,
+            TransitionTo = match.Groups["transitionTo"].Value,
+            NextName = match.Groups["nextName"].Value,
+            NextClip = match.Groups["nextClip"].Value
+        };
     }
 
     private void ListAnimatorStates()
